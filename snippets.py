75932055c34955dfe1ce6104ee14371473c43263
Eric Sokolov <eric.sokolov.55@gmail.com>
2017-07-01 13:22:36 -0700

    Added get subparser

--- snippets.py
+++ snippets.py
@@ -36,6 +36,9 @@ def main():
     put_parser.add_argument("name", help="Name of the snippet")
     put_parser.add_argument("snippet", help="Snippet text")
 
+    get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
+    get_parser.add_argument("name", help="Name of the snippet")
+
     arguments = parser.parse_args()
 
 if __name__ == "__main__":
