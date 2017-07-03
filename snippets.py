9065c4d2b0900447b930ad48c21ef05f5e01bcf0
Eric Sokolov <eric.sokolov.55@gmail.com>
2017-07-03 11:23:48 -0700

    Added functionality to put and get methods

--- snippets.py
+++ snippets.py
@@ -1,27 +1,42 @@
 import logging
 import argparse
+import psycopg2
 
 #Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
 
+#Connect to Snippets Database
+logging.debug("Connecting to PostgreSQL")
+connection = psycopg2.connect(database="snippets")
+logging.debug("Database connection established.")
+
+
 def put(name, snippet):
     """
     Store a snippet with an associated name.
-
     Returns the name and the snippet
     """
-    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name,snippet))
+    logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
+    cursor = connection.cursor()
+    command = "insert into snippets values (%s, %s)"
+    cursor.execute(command, (name, snippet))
+    connection.commit()
+    logging.debug("Snippet stored successfully.")
     return name, snippet
 
 def get(name):
     """Retrieve the snippet with a given name.
 
     If there is no such snippet, return '404: Snippet Not Found'.
-
     Returns the snippet.
     """
-    logging.error("FIXME: Unimplemented - get({!r})".format(name))
-    return ""
+    logging.info("Getting a snippet from keyword {!r}".format(name))
+    cursor = connection.cursor()
+    command = "select message from snippets where keyword = %s"
+    cursor.execute(command, (name,))
+    snippet = cursor.fetchone()[0]
+    logging.debug("Snippet retrieved successfully.")
+    return snippet
 
 def main():
     """main functions"""
